cde1e3c6d49feaaafc536baf2c50ae89aea0f97f
Valery Mendez <valery220995@gmail.com>
2017-07-07 12:13:11 -0600

    Initial project setup
    Setup project with gradle, create modules client and server

---
+++ server/src/main/java/com/gft/GiFT/GiFtApplication.java
@@ -0,0 +1,12 @@
+package com.gft.GiFT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiFtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiFtApplication.class, args);
+	}
+}
