716176bb15bdfa633b2f47db864f2129abbcb090
Valery Mendez <valery220995@gmail.com>
2017-07-12 11:39:16 -0600

    Delete out directory
    Add /out/ to .gitignore

--- out/production/server/db/v201707111111__create_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE t_project (
-  project_id INT NOT NULL AUTO_INCREMENT,
-  project_name VARCHAR(45) NOT NULL,
-  portfolio_id INT NULL,
-  release_pattern_id INT NOT NULL,
-  cycle_type_id INT NULL,
-  project_status INT NULL,
-  PRIMARY KEY (project_id)
-);
--- out/production/server/db/v201707120911__insert_projects.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO t_project(project_name, portfolio_id, release_pattern_id, cycle_type_id, project_status)
-VALUES ('GiFT', 1, 2, 3, 4);
