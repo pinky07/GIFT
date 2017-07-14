7276b324c470bcf3ada9f8d488d37a0d59b3345a
foza <francisco.zuniga@gft.com>
2017-07-14 08:41:48 -0600

    ESLint rules, removed webpack-dev-server

--- client/src/components/welcome/Welcome.jsx
+++ client/src/components/welcome/Welcome.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 export default class Welcome extends React.Component {
   render() {
     const hello = 'Hello World';
-    var xxx = "fasjfd";
     return (
       <div>{hello}</div>
     );
