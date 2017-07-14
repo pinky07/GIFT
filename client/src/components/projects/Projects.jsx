b0721e92738bc6c2fe599de6d93e4c3ee32cbc9d
David Bermudez <ddbd@gft.com>
2017-07-14 15:32:00 -0600

    Auto stash before merge of "master" and "origin/master"
    Updating state when requesting data to the endpoint

--- client/src/components/projects/Projects.jsx
+++ client/src/components/projects/Projects.jsx
@@ -6,33 +6,27 @@ export default class Projects extends React.Component {
     this.listItems = [];
   }
 
-  loadProjects() {
-    fetch('http://localhost:8080/api/v1/projects/portfolio/1')
-      .then((resp) => resp.json())
-      .then((data) => {
-        this.listItems = data.map((project) => {
-          return <li>{project.name}</li>
-        });
+
+  componentDidMount() {
+        fetch('http://localhost:8080/api/v1/projects/portfolio/1')
+          .then((resp) => resp.json())
+          .then((data) => {
+            if (data) {
+              this.listItems = data.map((project) => {
+                return <li key ={project.id}>{project.name}</li>;
+              });
+              this.setState({listItems: this.listItems});
+            }
       })
       .catch(() => {
       });
   }
 
-  componentDidMount() {
-    this.loadProjects();
-  }
-
   render() {
-    const list = [{"id":1,"name":"GiFT","portfolioId":1,"releasePatternId":2,"cycleTypeId":1,"projectStatus":2},{"id":2,"name":"Big Ball","portfolioId":1,"releasePatternId":1,"cycleTypeId":2,"projectStatus":1}];
-    debugger;
     return (
-      <div>
         <ul>
-          {list.map(function(item, index){
-            return <li key={ index }>{item.name}</li>;
-          })}
+          {this.listItems}
         </ul>
-      </div>
     );
   }
 }
