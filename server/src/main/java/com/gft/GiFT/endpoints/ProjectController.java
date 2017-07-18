7e5e7559e8d3a23e84ea0df3f996839cf426ab2d
Oscar <oscar.centeno@outlook.com>
2017-07-18 14:11:19 -0600

    Added some logging to the Project Controller. build.gradle now emits an executable jar file.

--- server/src/main/java/com/gft/GiFT/endpoints/ProjectController.java
+++ server/src/main/java/com/gft/GiFT/endpoints/ProjectController.java
@@ -2,6 +2,8 @@ package com.gft.GiFT.endpoints;
 
 import com.gft.GiFT.entities.Project;
 import com.gft.GiFT.repository.ProjectRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,18 @@ import java.util.List;
 @RequestMapping(value = "/api/v1/projects")
 public class ProjectController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private ProjectRepository repository;
 
     @GetMapping("/portfolio/{id}")
     public ResponseEntity<List<Project>> findAllProjectsByPortfolio(@PathVariable("id") final int id) {
+        logger.debug("This is a debug message: findAllProjectsByPortfolio " + id);
+        logger.info("This is an info message: findAllProjectsByPortfolio " + id);
+        logger.warn("This is a warn message: findAllProjectsByPortfolio " + id);
+        logger.error("This is an error message: findAllProjectsByPortfolio " + id);
+
         return new ResponseEntity<>(
                 repository.findByPortfolioId(id),
                 HttpStatus.OK);
