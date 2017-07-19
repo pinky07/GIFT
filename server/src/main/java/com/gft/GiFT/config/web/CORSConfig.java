4bf01b995fe22cadc99307a9e4762c88db3642c0
Valery Mendez <valery220995@gmail.com>
2017-07-19 09:58:24 -0600

    Add CORS Configuration
    Add CORSConfig class

---
+++ server/src/main/java/com/gft/GiFT/config/web/CORSConfig.java
@@ -0,0 +1,31 @@
+package com.gft.GiFT.config.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CORSConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // TODO define appropriate origin servers
+                registry.addMapping("/**")
+                        .allowedMethods(HttpMethod.GET.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.DELETE.name(),
+                                HttpMethod.OPTIONS.name())
+                        .allowedOrigins("*")
+                        .allowedHeaders("Content-Type");
+            }
+        };
+    }
+
+}
