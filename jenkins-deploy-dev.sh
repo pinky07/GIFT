0a2f6c435d810fbdfb9ad195301e4b5b44f6e5dc
Oscar <oscar.centeno@outlook.com>
2017-07-26 17:20:11 -0600

    Creating MySQL docker

--- jenkins-deploy-dev.sh
+++ jenkins-deploy-dev.sh
@@ -2,7 +2,22 @@
 #
 
 IMAGE_NAME='com.gft/gift:0.0.1-SNAPSHOT'
-echo 'Launching new container based on image' $IMAGE_NAME
 
-docker run -e 'SPRING_PROFILES_ACTIVE=default,container' -d -p 11010:8080 $IMAGE_NAME
+echo 'Creating MySQL image named gift-mysql'
+docker run -d \
+    --name gift-mysql \
+    -e MYSQL_ROOT_PASSWORD=root \
+    -e MYSQL_DATABASE=gift \
+    -e MYSQL_USER=root \
+    -e MYSQL_PASSWORD=root \
+    mysql:latest
+
+
+echo 'Linking containers...'
+docker run -it  \
+    --name $IMAGE_NAME \
+    --link gift-mysql:mysql \
+    -p 11010:8080 \
+    $IMAGE_NAME
+
 echo 'Success'
