fd7b938494d86dae283440da9f76eaa1c376a5a1
Oscar <oscar.centeno@outlook.com>
2017-07-26 16:46:19 -0600

    undo to command on waiting

--- jenkins-deploy-dev.sh
+++ jenkins-deploy-dev.sh
@@ -4,12 +4,6 @@
 IMAGE_NAME='com.gft/gift:0.0.1-SNAPSHOT'
 echo 'Launching new container based on image' $IMAGE_NAME
 
-CONTAINER_ID=`docker run -e 'SPRING_PROFILES_ACTIVE=default,container' -d -p 11010:8080 $IMAGE_NAME`
-echo 'Container ID' $CONTAINER_ID
-
-echo 'Waiting 30s'
-sleep 30s
-
-docker logs $CONTAINER_ID
+docker run -e 'SPRING_PROFILES_ACTIVE=default,dev' -d -p 11010:11010 $IMAGE_NAME
 
 echo 'Success'
