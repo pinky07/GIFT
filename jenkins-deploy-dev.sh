cb8d067fda232f0c6dae4574d9a2a4c918a69b5e
Oscar <oscar.centeno@outlook.com>
2017-07-26 16:57:19 -0600

    trying to fix encoding

--- jenkins-deploy-dev.sh
+++ jenkins-deploy-dev.sh
@@ -4,6 +4,5 @@
 IMAGE_NAME='com.gft/gift:0.0.1-SNAPSHOT'
 echo 'Launching new container based on image' $IMAGE_NAME
 
-docker run -e 'SPRING_PROFILES_ACTIVE=default,dev' -d -p 11010:11010 $IMAGE_NAME
-
-echo 'Success'
+docker run -e 'SPRING_PROFILES_ACTIVE=default,container' -d -p 8080:11000 $IMAGE_NAME
+echo 'Success'
