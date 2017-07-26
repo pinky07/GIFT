3a9407c03ea56fdde7d40ba2509464efbb402183
Oscar <oscar.centeno@outlook.com>
2017-07-26 17:07:08 -0600

    This binds port 8080 of the container to port 11010 of the host machine.

--- jenkins-deploy-dev.sh
+++ jenkins-deploy-dev.sh
@@ -4,5 +4,5 @@
 IMAGE_NAME='com.gft/gift:0.0.1-SNAPSHOT'
 echo 'Launching new container based on image' $IMAGE_NAME
 
-docker run -e 'SPRING_PROFILES_ACTIVE=default,container' -d -p 8080:11010 $IMAGE_NAME
+docker run -e 'SPRING_PROFILES_ACTIVE=default,container' -d -p 11010:8080 $IMAGE_NAME
 echo 'Success'
