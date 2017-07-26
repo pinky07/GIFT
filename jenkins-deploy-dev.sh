ce1dd5325841d238aabbaebd3df5150758a6e683
Oscar <oscar.centeno@outlook.com>
2017-07-26 15:58:51 -0600

    Set main class name.

--- jenkins-deploy-dev.sh
+++ jenkins-deploy-dev.sh
@@ -4,5 +4,12 @@
 IMAGE_NAME='com.gft/gift:0.0.1-SNAPSHOT'
 echo 'Launching new container based on image' $IMAGE_NAME
 
-docker run -e 'SPRING_PROFILES_ACTIVE=default,dev' -d -p 11010:11010 $IMAGE_NAME
-echo 'Success'
+CONTAINER_ID=`docker run -e 'SPRING_PROFILES_ACTIVE=default,dev' -d -p 11010:8080 $IMAGE_NAME`
+echo 'Container ID' $CONTAINER_ID
+
+echo 'Waiting 30s'
+sleep 30s
+
+docker logs $CONTAINER_ID
+
+echo 'Success'
