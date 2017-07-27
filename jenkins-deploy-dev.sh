e8e620c6f877a478506d31ab36d284026fe018d9
Oscar <oscar.centeno@outlook.com>
2017-07-26 20:41:54 -0600

    run the web app after linking with mysql container

--- jenkins-deploy-dev.sh
+++ jenkins-deploy-dev.sh
@@ -11,11 +11,6 @@ docker rm 'gift-app'
 
 IMAGE_NAME='com.gft/gift:0.0.1-SNAPSHOT'
 
-echo 'Running container... ' $IMAGE_NAME
-CONTAINER_ID=`docker run -e 'SPRING_PROFILES_ACTIVE=default,container' -d $IMAGE_NAME`
-
-echo 'Container ID:' $CONTAINER_ID
-
 echo 'Creating MySQL image named gift-mysql'
 docker run -d \
     --name gift-mysql \
@@ -26,11 +21,14 @@ docker run -d \
     mysql:latest
 
 echo 'Linking containers...'
-docker run -t  \
+CONTAINER_ID=`docker run -t  \
     --name gift-app \
+    -e 'SPRING_PROFILES_ACTIVE=default,container'
     --link gift-mysql:mysql \
     -p 11010:8080 \
-    $IMAGE_NAME
+    $IMAGE_NAME`
+
+echo 'Container ID:' $CONTAINER_ID
 
 echo 'Waiting 30s'
 sleep 30s
