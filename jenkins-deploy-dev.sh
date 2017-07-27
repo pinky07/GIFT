93080f1a4b9e580d389a0f3de8428f1da07622d1
Oscar <oscar.centeno@outlook.com>
2017-07-26 20:33:12 -0600

    trying ton integrate flyway in containers

--- jenkins-deploy-dev.sh
+++ jenkins-deploy-dev.sh
@@ -51,3 +51,19 @@ docker logs gift-mysql
 echo 'Success'
 
 cat /etc/hosts
+
+
+echo 'Flyway...'
+apt-get install -y curl
+curl -OL https://bintray.com/artifact/download/business/maven/flyway-commandline-3.2.1-linux-x64.tar.gz
+tar -xzf flyway-commandline-3.2.1-linux-x64.tar.gz
+rm flyway-commandline-3.2.1-linux-x64.tar.gz
+chmod +x flyway-3.2.1/flyway
+
+docker run -e FLYWAY_COMMAND=migrate --link link gift-mysql:mysql -v /var/lib/jenkins/workspace/GiFT-App/database/db/:/migrations $IMAGE_NAME
+
+if [ "$FLYWAY_URL" = "" ]; then
+	FLYWAY_URL="jdbc:mysql://gift-mysql:3306/gift?useSSL=false"
+fi
+
+flyway-3.2.1/flyway $FLYWAY_COMMAND -url=$FLYWAY_URL -user=root -password=root -baselineOnMigrate=true -locations=filesystem:/migrations
