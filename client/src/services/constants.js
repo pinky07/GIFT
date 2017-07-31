d31498f3593c5ae6d8cf5388ac0502491166abfa
Oscar <oscar.centeno@outlook.com>
2017-07-31 16:06:59 -0600

    Testing using criprj again

--- client/src/services/constants.js
+++ client/src/services/constants.js
@@ -1,6 +1,6 @@
 export const constants = {
   // API: 'http://localhost:8080/api/v1'
-  API: 'http://10.200.202.122:11010/api/v1'
+  API: 'http://criprj:11010/api/v1'
 }
 
 export default constants;
