8ac2759916170c6556f3bdeee04db3bc244c9a85
Oscar <oscar.centeno@outlook.com>
2017-08-02 20:38:47 -0600

    Commiting UI validations

--- client/src/components/projects/cycleSnapAdd/CycleSnapAdd.js
+++ client/src/components/projects/cycleSnapAdd/CycleSnapAdd.js
@@ -22,20 +22,17 @@ export default class CycleSnapAdd extends React.Component {
     super(props);
 
     this._onSubmit = this._onSubmit.bind(this);
+    this._validateFields = this._validateFields.bind(this);
 
     this.state = {
-      successMessage: false,
-      errorMessageOnSubmit: '',
-      isLoadingTheFirstTime: true,
       projectId: props.params.id,
       projectName: '',
-      cycleSnapName: '',
-      cycleSnapStartDate: '',
-      cycleSnapEndDate: '',
+      name: '',
+      startDate: '',
+      endDate: '',
       targetedPoints: 0,
       achievedPoints: 0,
-      errorMessage: '',
-      noErrors: true,
+      errorMessage: undefined,
       errors: {}
     };
   }
@@ -48,7 +45,6 @@ export default class CycleSnapAdd extends React.Component {
     if (isNaN(projectId)) {
       this.setState({
         errorMessage: "Invalid project id",
-        isLoadingTheFirstTime: false
       });
     }
     else {
@@ -56,21 +52,19 @@ export default class CycleSnapAdd extends React.Component {
         if (response.data) {
           this.setState({
             projectName: response.data,
-            isLoadingTheFirstTime: false
           });
           // this.setState({ projectName: 'MyProject' });
         }
       }).catch((error) => {
         this.setState({
           errorMessage: error.response.data.message,
-          isLoadingTheFirstTime: false
         });
       });
     }
   }
 
   isValidDate(text) {
-    let aDate = new Date(text + "T00:00");
+    let aDate = new Date(text + "T18:00");
 
     if (Object.prototype.toString.call(aDate) === "[object Date]") {
       // it is a date
@@ -80,7 +74,13 @@ export default class CycleSnapAdd extends React.Component {
       }
       else {
         // date is valid
-        return true;
+        let dateReg = /^\d{4}\-\d{2}\-\d{2}$/;
+        if (dateReg.test(text)) {
+          return true;
+        }
+        else {
+          return false;
+        }
       }
     }
     else {
@@ -89,38 +89,103 @@ export default class CycleSnapAdd extends React.Component {
     }
   }
 
-  _changeCycleName(index) {
+  _haveErrors(errors) {
+    if (errors.name ||
+      errors.startDate ||
+      errors.endDate ||
+      errors.achievedPoints ||
+      errors.targetedPoints) {
+      return true;
+    }
+    else
+      return false;
+  }
+
+  _changeName(index) {
     return (event) => {
+      // Get state
       let newValue = event.target.value;
-      this.setState({ cycleSnapName: newValue });
+
+      // Set new state
+      let { errors } = this.state;
+
+      // Validate
+      errors.name = this._validateName(newValue);
+
+      this.setState({
+        errors: errors,
+        name: newValue
+      });
     };
   }
 
   _changeStartDate(index) {
-    return (newDate) => {
-      this.setState({ cycleSnapStartDate: newDate });
+    return (newValue) => {
+      // Get state
+      let { errors } = this.state;
+
+      // Validate
+      errors.startDate = this._validateStartDate(newValue);
+
+      // Set new state
+      this.setState({
+        errors: errors,
+        startDate: newValue
+      });
     }
   }
 
   _changeEndDate(index) {
-    return (newDate) => {
-      this.setState({ cycleSnapEndDate: newDate });
+    return (newValue) => {
+      // Get state
+      let { errors } = this.state;
+      let { startDate } = this.state;
+
+      // Validate
+      errors.endDate = this._validateEndDate(startDate, newValue);
+
+      // Set new state
+      this.setState({
+        errors: errors,
+        endDate: newValue
+      });
     }
   }
 
   _changeTargetedPoints(index) {
     return (event) => {
-      let value = event.target.value;
-      value = parseInt(value, 10);
-      this.setState({ targetedPoints: value });
+      // Get state
+      let newValue = event.target.value;
+
+      if (!isNaN(newValue)) {
+        let { errors } = this.state;
+
+        // Validate
+        errors.targetedPoints = this._validateTargetedPoints(newValue);
+
+        // Set new state
+        this.setState({
+          errors: errors,
+          targetedPoints: newValue
+        });
+      }
     };
   }
 
   _changeAchievedPoints(index) {
     return (event) => {
-      let value = event.target.value;
-      value = parseInt(value, 10);
-      this.setState({ achievedPoints: value });
+      // Get state
+      let newValue = event.target.value;
+      let { errors } = this.state;
+
+      // Validate
+      errors.achievedPoints = this._validateAchievedPoints(newValue);
+
+      // Set new state
+      this.setState({
+        errors: errors,
+        achievedPoints: newValue
+      });
     };
   }
 
@@ -128,189 +193,212 @@ export default class CycleSnapAdd extends React.Component {
     return new Date(endDate) > new Date(startDate); // true if endDate is later
   }
 
-  validateFields() {
+  _validateName(name) {
+    let error = undefined;
 
+    if (!name) {
+      error = 'Name is required';
+    }
+    else {
+      if (name.length > 200) {
+        error = 'Name has a max of 200 characters';
+      }
+    }
+
+    return error;
+  }
+
+  _validateStartDate(startDate) {
+    let error = undefined;
+
+    if (!startDate) {
+      error = 'Start date is required';
+    }
+    else {
+      if (!this.isValidDate(startDate)) {
+        error = 'Start date should be a valid one';
+      }
+    }
+
+    return error;
+  }
+
+  _validateEndDate(startDate, endDate) {
+    let error = undefined;
+
+    if (!endDate) {
+      error = 'End date is required';
+    }
+    else {
+      if (!this.isValidDate(endDate)) {
+        error = 'End date should be a valid one';
+      }
+    }
+
+    if (this.isValidDate(startDate) && this.isValidDate(endDate)) {
+      if (!this.startPrecedesEndDate(startDate, endDate)) {
+        error = 'End date should happen after the start date';
+      }
+    }
+
+    return error;
+  }
+
+  _validateTargetedPoints(points) {
+    let error = undefined;
+
+    if (points == '') {
+      error = 'Targeted points should be a number';
+    }
+    else {
+      if (points < 0) {
+        error = 'Targeted points min is 0';
+      }
+      if (points > 10000) {
+        error = 'Targeted points max is 10,000';
+      }
+    }
+
+    return error;
+  }
+
+  _validateAchievedPoints(points) {
+    let error = undefined;
+
+    if (points == '') {
+      error = 'Achieved points is required';
+    }
+    else {
+      if (points < 0) {
+        error = 'Targeted points min is 0';
+      }
+
+      if (points > 10000) {
+        error = 'Achieved points max is 10,000';
+      }
+    }
+
+    return error;
+  }
+
+  _validateFields() {
     let errors = {};
-    let noErrors = true;
 
-    const { cycleSnapName } = this.state;
-    if (!cycleSnapName) {
-      errors.cycleSnapName = 'Name is required';
-      noErrors = false;
-    }
-    else {
-      if (cycleSnapName.length > 200) {
-        errors.cycleSnapName = 'Name has a max of 200 characters';
-        noErrors = false;
-      }
-    }
+    const { name } = this.state;
+    errors.name = this._validateName(name);
 
-    const { cycleSnapStartDate } = this.state;
-    if (!cycleSnapStartDate) {
-      errors.cycleSnapStartDate = 'Start date is required';
-      noErrors = false;
-    }
-    else {
-      if (!this.isValidDate(cycleSnapStartDate)) {
-        errors.cycleSnapStartDate = 'Start date should be a valid one';
-        noErrors = false;
-      }
-    }
+    const { startDate } = this.state;
+    errors.startDate = this._validateStartDate(startDate);
 
-    const { cycleSnapEndDate } = this.state;
-    if (!cycleSnapEndDate) {
-      errors.cycleSnapEndDate = 'End date is required';
-      noErrors = false;
-    }
-    else {
-      if (!this.isValidDate(cycleSnapEndDate)) {
-        errors.cycleSnapEndDate = 'End date should be a valid one';
-        noErrors = false;
-      }
-    }
-
-    if (this.isValidDate(cycleSnapStartDate) && this.isValidDate(cycleSnapEndDate)) {
-      if (!this.startPrecedesEndDate(cycleSnapStartDate, cycleSnapEndDate)) {
-        errors.cycleSnapEndDate = 'End date should happen after the start date';
-        noErrors = false;
-      }
-    }
+    const { endDate } = this.state;
+    errors.endDate = this._validateEndDate(startDate, endDate);
 
     const { targetedPoints } = this.state;
-    if (!targetedPoints) {
-      errors.targetedPoints = 'Targeted points is required';
-      noErrors = false;
-    }
-    else {
-      if (targetedPoints > 10000) {
-        errors.targetedPoints = 'Targeted points max is 10,000';
-        noErrors = false;
-      }
-    }
+    errors.targetedPoints = this._validateTargetedPoints(targetedPoints);
 
     const { achievedPoints } = this.state;
-    if (!achievedPoints) {
-      errors.achievedPoints = 'Achieved points is required';
-      noErrors = false;
-    }
-    else {
-      if (achievedPoints > 10000) {
-        errors.achievedPoints = 'Achieved points max is 10,000';
-        noErrors = false;
-      }
-    }
+    errors.achievedPoints = this._validateAchievedPoints(achievedPoints);
 
-    this.setState({ noErrors: noErrors, errors: errors });
-
-    return noErrors;
+    return errors;
   }
 
   _onSubmit(event) {
     event.preventDefault();
 
-    let noErrors = this.validateFields();
+    let errors = this._validateFields();
+    let noErrors = !this._haveErrors(errors);
 
     if (noErrors) {
       axios.post(`${constants.API}/projects/cyclesnaps`, {
         projectId: this.state.projectId,
-        cycleSnapName: this.state.cycleSnapName,
-        startDate: this.state.cycleSnapStartDate,
-        endDate: this.state.cycleSnapEndDate,
+        cycleSnapName: this.state.name,
+        startDate: this.state.startDate,
+        endDate: this.state.endDate,
         targetedPoints: this.state.targetedPoints,
         achievedPoints: this.state.achievedPoints
       })
         .then(function (response) {
-          this.setState({
-            successMessage: true
-          });
+          alert('Success! You added a cycle snap.');
         })
         .catch(function (error) {
-          console.log('error:' + error);
-          this.setState({
-            successMessage: false,
-            errorMessageOnSubmit: error
-          });
+          console.log('error:' + error.message);
+          alert('We got a bit of an issue here... ' + error.message);
         });
+    }
+    else {
+      this.setState({
+        errors: errors,
+      });
+
     }
   }
 
 
   render() {
-    const { successMessage } = this.state;
-
     const { errorMessage } = this.state;
-    const { projectName } = this.state;
-    const { isLoadingTheFirstTime } = this.state;
-
-    if (isLoadingTheFirstTime) {
+    if (errorMessage) {
       return <div>
         <h1>Add Cycle Snap</h1>
-        <h3>Loading...</h3>
+        <h3><Status value='critical' /> <span>{errorMessage}</span></h3>
       </div>
     }
     else {
+      const { projectName } = this.state;
 
-      if (errorMessage) {
-        return <div>
-          <h1>Add Cycle Snap</h1>
-          <h3><Status value='critical' /> <span>{errorMessage}</span></h3>
+      if (projectName) {
+
+        // if (successMessage) {
+        //   let notificationOnSubmitBox = <Notification message='Success! You just added a new cycle snap.' status='ok' size='small' />
+        // }
+        // else {
+        //   if (errorMessageOnSubmit) {
+        //     let notificationOnSubmitBox = <Notification message={`Oops! ' + ${error}`} status='critical' size='small' />
+        //   }
+        // }
+
+        const { errors } = this.state;
+
+        const { name } = this.state;
+        const { startDate } = this.state;
+        const { endDate } = this.state;
+        const { targetedPoints } = this.state;
+        const { achievedPoints } = this.state;
+
+        return <div id="layout-content" className="layout-content-wrapper">
+          <h1>Cycle Snap</h1>
+          <h2>Project: {projectName}</h2>
+
+          <Form compact={false}>
+            <FormField label='Cycle name or id' htmlFor="cycleNameInput" error={errors.name}>
+              <TextInput id='cycleNameInput' onDOMChange={this._changeName()} value={name} />
+            </FormField>
+            <FormField label='Start Date' htmlFor="cycleStartDate" error={errors.startDate}>
+              <DateTime id='cycleStartDate' format='YYYY-MM-DD' value={startDate} onChange={this._changeStartDate()} />
+            </FormField>
+            <FormField label='End Date' htmlFor="cycleEndDate" error={errors.endDate}>
+              <DateTime id='cycleEndDate' format='YYYY-MM-DD' value={endDate} onChange={this._changeEndDate()} />
+            </FormField>
+            <FormField label='Targeted points' htmlFor='targetedPointsId' error={errors.targetedPoints}>
+              <NumberInput id='targetedPointsId' step={1} value={targetedPoints} min={0} onChange={this._changeTargetedPoints()} />
+            </FormField>
+            <FormField label='Achieved points' htmlFor='achievedPointsId' error={errors.achievedPoints}>
+              <NumberInput id='achievedPoints' step={1} value={achievedPoints} min={0} onChange={this._changeAchievedPoints()} />
+            </FormField>
+
+
+            <Footer pad={{ "vertical": "medium" }}>
+              <Button label='Add' type='submit' primary={true} onClick={this._onSubmit} />
+            </Footer>
+
+            {/* {notificationOnSubmitBox} */}
+          </Form>
         </div>
       }
       else {
-
-        if (projectName) {
-
-          // if (successMessage) {
-          //   let notificationOnSubmitBox = <Notification message='Success! You just added a new cycle snap.' status='ok' size='small' />
-          // }
-          // else {
-          //   if (errorMessageOnSubmit) {
-          //     let notificationOnSubmitBox = <Notification message={`Oops! ' + ${error}`} status='critical' size='small' />
-          //   }
-          // }
-
-          const { targetedPoints } = this.state;
-          const { achievedPoints } = this.state;
-          const { cycleSnapStartDate } = this.state;
-          const { cycleSnapEndDate } = this.state;
-          const { noErrors } = this.state;
-          const { errors } = this.state;
-
-          return <div id="layout-content" className="layout-content-wrapper">
-            <h1>Cycle Snap</h1>
-            <h2>Project: {projectName}</h2>
-
-            <Form compact={false}>
-              <FormField label='Cycle name or id' htmlFor="cycleNameInput" error={errors.cycleSnapName}>
-                <TextInput id='cycleNameInput' onDOMChange={this._changeCycleName()} />
-              </FormField>
-              <FormField label='Start Date' htmlFor="cycleStartDate" error={errors.cycleSnapStartDate}>
-                <DateTime id='cycleStartDate' format='YYYY-MM-DD' value={cycleSnapStartDate} onChange={this._changeStartDate()} />
-              </FormField>
-              <FormField label='End Date' htmlFor="cycleEndDate" error={errors.cycleSnapEndDate}>
-                <DateTime id='cycleEndDate' format='YYYY-MM-DD' value={cycleSnapEndDate} onChange={this._changeEndDate()} />
-              </FormField>
-              <FormField label='Targeted points' htmlFor='targetedPointsId' error={errors.targetedPoints}>
-                <NumberInput id='targetedPointsId' step={1} value={targetedPoints} min={0} onChange={this._changeTargetedPoints()} />
-              </FormField>
-              <FormField label='Achieved points' htmlFor='achievedPointsId' error={errors.achievedPoints}>
-                <NumberInput id='achievedPoints' step={1} value={achievedPoints} min={0} onChange={this._changeAchievedPoints()} />
-              </FormField>
-
-              <Footer pad={{ "vertical": "medium" }}>
-                <Button label='Add' type='submit' primary={true} onClick={this._onSubmit} />
-              </Footer>
-
-              {/* {notificationOnSubmitBox} */}
-            </Form>
-          </div>
-        }
-        else {
-          return <div id="layout-content" className="layout-content-wrapper">
-            <h1>Add Cycle Snap for project Id #{projectName}</h1>
-            <h3><Status value='unknown' /> <span>We are sorry, but this project does not exist.</span></h3>
-          </div>
-        }
+        return <div id="layout-content" className="layout-content-wrapper">
+          <h1>Add Cycle Snap for project Id #{projectName}</h1>
+          <h3><Status value='unknown' /> <span>We are sorry, but this project does not exist.</span></h3>
+        </div>
       }
     }
   }
