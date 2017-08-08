40abdf6b120298c2b3e26cc14b0714a2daec271c
Valery Mendez <valery220995@gmail.com>
2017-08-08 08:59:44 -0600

    Update service to add new field to the CycleSnapSTO

--- server/src/main/java/com/gft/GiFT/projects/dashboard/DefaultProjectService.java
+++ server/src/main/java/com/gft/GiFT/projects/dashboard/DefaultProjectService.java
@@ -4,7 +4,11 @@ import com.gft.GiFT.entities.CycleSnap;
 import com.gft.GiFT.entities.Project;
 import com.gft.GiFT.formatters.DateFormatter;
 import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 public class DefaultProjectService implements ProjectService {
@@ -17,7 +21,7 @@ public class DefaultProjectService implements ProjectService {
 
 
     @Override
-    public ProjectDTO findDashboardByProjectId(int projectId) {
+    public ProjectDTO findDashboardByProjectId(int projectId) throws ParseException {
 
         Project project = projectRepository.findOne(projectId);
 
@@ -27,24 +31,31 @@ public class DefaultProjectService implements ProjectService {
         ProjectDTO projectDTO = new ProjectDTO();
         projectDTO.setName(project.getName());
 
-        if(project.getCycleSnapSet().isEmpty()){
+        List<String> releaseSnapDates = new LinkedList<>();
+
+        for (ReleaseSnap releaseSnap : project.getReleaseSnaps()) {
+            releaseSnapDates.add(releaseSnap.getReleaseDate());
+        }
+
+        if (project.getCycleSnapSet().isEmpty()) {
             projectDTO.setCycleSnaps(new LinkedHashSet<>());
         } else {
             projectDTO.setCycleSnaps(new LinkedHashSet<>());
             for (CycleSnap cycleSnap : project.getCycleSnapSet()) {
-                projectDTO.getCycleSnaps().add(createCycleSnapDTO(cycleSnap));
+                projectDTO.getCycleSnaps().add(createCycleSnapDTO(cycleSnap, releaseSnapDates));
             }
         }
         return projectDTO;
     }
 
-    private CycleSnapDTO createCycleSnapDTO(CycleSnap cycleSnap) {
+    private CycleSnapDTO createCycleSnapDTO(CycleSnap cycleSnap, List<String> releaseDates) throws ParseException {
         CycleSnapDTO cycleSnapDTO = new CycleSnapDTO();
         cycleSnapDTO.setCycleSnapName(cycleSnap.getCycleSnapName());
         cycleSnapDTO.setStartDate(cycleSnap.getStartDate());
         cycleSnapDTO.setEndDate(cycleSnap.getEndDate());
         cycleSnapDTO.setTargetedPoints(cycleSnap.getTargetedPoints());
         cycleSnapDTO.setAchievedPoints(cycleSnap.getAchievedPoints());
+        cycleSnapDTO.setDaysSinceLastRelease(DaysSinceLastReleaseCalculation.determineDaysSinceLastRelease(cycleSnap.getEndDate(), releaseDates));
 
         if (cycleSnap.getTargetedPoints() == 0) {
             cycleSnapDTO.setTac("No Data");
--- server/src/main/java/com/gft/GiFT/projects/dashboard/ProjectService.java
+++ server/src/main/java/com/gft/GiFT/projects/dashboard/ProjectService.java
@@ -1,6 +1,8 @@
 package com.gft.GiFT.projects.dashboard;
 
+import java.text.ParseException;
+
 public interface ProjectService {
 
-    ProjectDTO findDashboardByProjectId(int id);
+    ProjectDTO findDashboardByProjectId(int id) throws ParseException;
 }
