344879405763d2c9fd116a6d0a9cc658135feb62
pinky <Pinky.Agrawal@gft.com>
2017-08-08 14:03:38 -0600

    eEntity for incidents class

---
+++ server/src/main/java/com/gft/GiFT/projects/dashboard/IncidentsReport.java
@@ -0,0 +1,25 @@
+package com.gft.GiFT.projects.dashboard;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table( name = "t_incidents_report" )
+public class IncidentsReport {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "report_id")
+    private int id;
+
+    @Column(name = "report_date", columnDefinition = "DATE", nullable = false)
+    private String incidentsDate;
+
+    @Column(name = "total_incidents", columnDefinition = "INT", nullable = false)
+    private int totalIncidents;
+
+    @Column(name = "projectId", columnDefinition = "INT", nullable = false)
+    private int projectId;
+
+}
