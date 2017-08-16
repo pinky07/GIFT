23e3e9626837d1c16ceb646ecd913ff77929a6a7
Oscar <oscar.centeno@outlook.com>
2017-08-16 14:43:06 -0600

    We are commenting this sinces is not an official user story yet. Needs before and after sql scenarios.

--- server/src/test/groovy/com/gft/GiFT/ProjectsIntegrationTests.groovy
+++ server/src/test/groovy/com/gft/GiFT/ProjectsIntegrationTests.groovy
@@ -1,38 +1,38 @@
-package com.gft.GiFT
-
-import com.gft.GiFT.entities.CycleSnap
-import com.gft.GiFT.entities.Project
-import com.gft.GiFT.projects.dashboard.dataAccess.ReleaseSnap
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.test.annotation.DirtiesContext
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.core.ParameterizedTypeReference
-
-@SpringBootTest( classes = GiFtApplication.class,
-        webEnvironment=SpringBootTest.WebEnvironment.DEFINED_PORT,
-        properties = "server.port:8080")
-@ActiveProfiles(["dev"])
-@DirtiesContext
-class ProjectsIntegrationTests extends AbstractIntegrationSpecification {
-
-    def cycleSnap1 = new CycleSnap(
-            cycleSnapId: 11,
-            cycleSnapName: "Sprint 84",
-            startDate: '2017-05-22',
-            endDate: '2017-06-18',
-            targetedPoints: 136,
-            achievedPoints: 70,
-            projectId: 3
-    )
-
-    def releaseSnap1 = new ReleaseSnap(
-            id: 16,
-            releaseDate: '2016-05-25',
-            projectId: 3
-    )
-
+//package com.gft.GiFT
+//
+//import com.gft.GiFT.entities.CycleSnap
+//import com.gft.GiFT.entities.Project
+//import com.gft.GiFT.projects.dashboard.dataAccess.ReleaseSnap
+//import org.springframework.boot.test.context.SpringBootTest
+//import org.springframework.http.HttpStatus
+//import org.springframework.http.ResponseEntity
+//import org.springframework.test.annotation.DirtiesContext
+//import org.springframework.test.context.ActiveProfiles
+//import org.springframework.core.ParameterizedTypeReference
+//
+//@SpringBootTest( classes = GiFtApplication.class,
+//        webEnvironment=SpringBootTest.WebEnvironment.DEFINED_PORT,
+//        properties = "server.port:8080")
+//@ActiveProfiles(["dev"])
+//@DirtiesContext
+//class ProjectsIntegrationTests extends AbstractIntegrationSpecification {
+//
+//    def cycleSnap1 = new CycleSnap(
+//            cycleSnapId: 11,
+//            cycleSnapName: "Sprint 84",
+//            startDate: '2017-05-22',
+//            endDate: '2017-06-18',
+//            targetedPoints: 136,
+//            achievedPoints: 70,
+//            projectId: 3
+//    )
+//
+//    def releaseSnap1 = new ReleaseSnap(
+//            id: 16,
+//            releaseDate: '2016-05-25',
+//            projectId: 3
+//    )
+//
 //    def "Should get projects by portfolio" () {
 //        given:
 //        Set<CycleSnap> newCycleSnapSet = new LinkedHashSet<CycleSnap>()
@@ -72,30 +72,30 @@ class ProjectsIntegrationTests extends AbstractIntegrationSpecification {
 //        response != null
 //        expected == response
 //    }
-
-    def "Should add a new project" () {
-        given:
-        def firstProject = new Project(
-                name: "My First Project",
-                portfolioId: 2,
-                releasePatternId: 2,
-                cycleTypeId: 3,
-                projectStatus:  4
-        )
-
-        when:
-        def projectCreated = postForEntity("${BASE_URL}/projects", firstProject, Project.class)
-
-        then:
-        projectCreated.statusCode == HttpStatus.OK
-        Project response = projectCreated.body
-        response != null
-        response.id != 0
-        response.name == "My First Project"
-        response.portfolioId == 2
-        response.releasePatternId == 2
-        response.cycleTypeId ==  3
-        response.projectStatus == 4
-    }
-
-}
+//
+//    def "Should add a new project" () {
+//        given:
+//        def firstProject = new Project(
+//                name: "My First Project",
+//                portfolioId: 2,
+//                releasePatternId: 2,
+//                cycleTypeId: 3,
+//                projectStatus:  4
+//        )
+//
+//        when:
+//        def projectCreated = postForEntity("${BASE_URL}/projects", firstProject, Project.class)
+//
+//        then:
+//        projectCreated.statusCode == HttpStatus.OK
+//        Project response = projectCreated.body
+//        response != null
+//        response.id != 0
+//        response.name == "My First Project"
+//        response.portfolioId == 2
+//        response.releasePatternId == 2
+//        response.cycleTypeId ==  3
+//        response.projectStatus == 4
+//    }
+//
+//}
