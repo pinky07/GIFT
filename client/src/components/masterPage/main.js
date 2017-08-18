f4838a2ff36e15f3f7cda9cff71eb25a28856e21
Valery Mendez <valery220995@gmail.com>
2017-08-18 15:45:23 -0600

    Remove unused imports and do pretty format

--- client/src/components/masterPage/main.js
+++ client/src/components/masterPage/main.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
-import App  from 'grommet/components/App';
-import Box  from 'grommet/components/Box';
-import Section  from 'grommet/components/Section';
+import React, {Component} from 'react';
+import App from 'grommet/components/App';
+import Box from 'grommet/components/Box';
 
 import AppHeader from './appHeader/AppHeader';
-import AppFooter from './appFooter/AppFooter';
 
 export default class Main extends Component {
   constructor() {
@@ -14,11 +12,11 @@ export default class Main extends Component {
   render() {
     return (
       <App centered={false}>
-        <Box align="stretch" flex="grow" >
-        <AppHeader/>
+        <Box align="stretch" flex="grow">
+          <AppHeader/>
           <Box pad={{horizontal: 'medium', vertical: 'medium'}}>
-          {this.props.children}
-        </Box>
+            {this.props.children}
+          </Box>
         </Box>
       </App>
     );
