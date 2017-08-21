4046e44deb486d59f5489e842f10b015584f873e
Oscar <oscar.centeno@outlook.com>
2017-08-20 19:41:38 -0600

    Commit to correct testing url

--- client/src/components/constants.js
+++ client/src/components/constants.js
@@ -1,6 +1,6 @@
 export const constants = {
-  API: 'http://localhost:8080/api/v1'
-  // API: 'http://criprj:11010/api/v1'
+  // API: 'http://localhost:8080/api/v1'
+  API: 'http://criprj:11010/api/v1'
   // API: 'http://gift-demo.herokuapp.com/api/v1'
 }
 
