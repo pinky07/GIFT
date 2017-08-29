ca419ebde68c92edfc8fce8acb95be70841c1b03
Oscar <oscar.centeno@outlook.com>
2017-08-29 00:58:42 -0600

    jenkins build

--- client/src/components/constants.js
+++ client/src/components/constants.js
@@ -1,9 +1,26 @@
 export const constants = {
+  getApi(){
+    let host = window.location.hostname;
+    if (host == "172.17.0.3"){
+      console.log("host: " + host);
+      return "http://172.17.0.3:8080/api/v1"
+    }
+    else{
+
+      if (host == "localhost"){
+        console.log("host: " + host);
+        return "http://localhost:8080/api/v1"
+      }
+      else {
+        return "http://criprj:11010/api/v1";
+      }
+    }
+  },
   // API: 'http://localhost:8080/api/v1',
     API: 'http://criprj:11010/api/v1',
   // API: 'http://gift-demo.herokuapp.com/api/v1'
 
-  Website: 'http://localhost:11010/#'
+  Website: 'http://172.17.0.3:8080/#'
 }
 
 export default constants;
--- client/src/components/projects/dashboard/dashboardService.js
+++ client/src/components/projects/dashboard/dashboardService.js
@@ -2,7 +2,7 @@ import constants from '../../constants';
 
 const dashboardService = {
   load(getProjectDashboardCallback, projectId, onSuccessCallback, onErrorCallback) {
-    return getProjectDashboardCallback(`${constants.API}/projects/${projectId}/dashboard`)
+    return getProjectDashboardCallback(`${constants.getApi()}/projects/${projectId}/dashboard`)
       .then((response) => onSuccessCallback(response))
       .catch((error) => onErrorCallback(error));
   }
